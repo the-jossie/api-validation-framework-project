c08b90da57e98a1711932cd37b0de6ec4e4e32a9
Dev Jo <idowuayanfeoluwa@gmail.com>
2025-08-08 21:33:57 +0100

    feat: added valid order integration test

--- ApiValidationFramework.Tests/OrderControllerTests.cs
+++ ApiValidationFramework.Tests/OrderControllerTests.cs
@@ -26,4 +26,19 @@ public class OrderControllerTests : IClassFixture<WebApplicationFactory<Program>
 
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
+
+    [Fact]
+    public async Task PostOrder_ValidOrder_ReturnsOk()
+    {
+        var order = new {
+            productName = "Nike AF1",
+            quantity = 10,
+            startDate = "2025-08-01",
+            endDate = "2025-08-10"
+        };
+
+        var response = await _client.PostAsJsonAsync("/order", order);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
 }
